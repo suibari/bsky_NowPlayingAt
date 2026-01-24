9983d8dc011c57fffdc8b94a2af101367b7f1b93
suibari <404.not.found.address@gmail.com>
2026-01-24 12:57:23 +0900

    Spotify, YTMusicアイコン置き換え

--- src/lib/components/TrackCard.svelte
+++ src/lib/components/TrackCard.svelte
@@ -160,7 +160,19 @@
         {#if resolvingLink === "spotify"}
           <Loader2 size={20} class="animate-spin" />
         {:else}
-          <Disc size={20} />
+          <!-- Spotify Icon -->
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            class="transform scale-110"
+          >
+            <path
+              d="M12 0C5.4 0 0 5.4 0 12s5.4 12 12 12 12-5.4 12-12S18.66 0 12 0zm5.521 17.34c-.24.359-.66.48-1.021.24-2.82-1.74-6.36-2.101-10.561-1.141-.418.122-.779-.179-.899-.539-.12-.421.18-.78.54-.9 4.56-1.021 8.52-.6 11.64 1.32.42.18.479.659.301 1.02zm1.44-3.3c-.301.42-.841.6-1.262.3-3.239-1.98-8.159-2.58-11.939-1.38-.479.12-1.02-.12-1.14-.6-.12-.48.12-1.021.6-1.141C9.6 9.9 15 10.561 18.72 12.84c.361.181.54.78.241 1.2zm.12-3.36C15.24 8.4 8.82 8.16 5.16 9.301c-.6.179-1.2-.181-1.38-.721-.18-.601.18-1.2.72-1.381 4.26-1.26 11.4-1.02 16.141 1.8.539.3.719 1.02.419 1.56-.299.421-1.02.599-1.559.3z"
+            />
+          </svg>
         {/if}
       </button>
 
@@ -173,20 +185,19 @@
         {#if resolvingLink === "ytmusic"}
           <Loader2 size={20} class="animate-spin" />
         {:else}
+          <!-- YouTube Music Icon -->
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="20"
             height="20"
             viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            ><circle cx="12" cy="12" r="10" /><polygon
-              points="10 8 16 12 10 16 10 8"
-            /></svg
+            fill="currentColor"
+            class="transform scale-125"
           >
+            <path
+              d="M12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0zm0 19.2c-3.977 0-7.2-3.223-7.2-7.2s3.223-7.2 7.2-7.2 7.2 3.223 7.2 7.2-3.223 7.2-7.2 7.2zm-2.4-10.8v7.2l6-3.6z"
+            />
+          </svg>
         {/if}
       </button>
 
