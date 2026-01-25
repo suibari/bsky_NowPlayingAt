0a1492280ebc4517137241541f163a3f1d99bb19
suibari <404.not.found.address@gmail.com>
2026-01-25 10:33:28 +0900

    PWA対応

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,31 @@
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({ plugins: [tailwindcss(), sveltekit()] });
+export default defineConfig({
+    plugins: [
+        tailwindcss(),
+        sveltekit(),
+        SvelteKitPWA({
+            manifest: {
+                name: 'なうぷれあっと',
+                short_name: 'NowPlayingAt',
+                description: '音楽共有ATprotoサービス',
+                theme_color: '#ffffff',
+                icons: [
+                    {
+                        src: 'favicon.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'favicon.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    }
+                ]
+            }
+        })
+    ]
+});
